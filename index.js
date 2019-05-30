3cf3f79b1da4a9f259392d8f7235222c31b170fc
madov68 <matheus.oliveirav@gmail.com>
2019-05-29 22:24:51 -0300

    adding bootstrap and other funcionalities

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 3000
 const pessoas = require('./routes/pessoas')
 
 app.use(express.static('public'))
+app.get('/', (req, res) => res.render('index'))
 app.use('/pessoas', pessoas)
-app.set('view', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.listen(port, () => console.log('Connected'))
